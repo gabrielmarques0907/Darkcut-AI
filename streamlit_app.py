29ada770bbc0032aab6b4b591591b4973f4a8dc0
gabrielmarques0907 <gabrielmarques200511@gmail.com>
2026-08-29 14:34:41 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import whisper
+import tempfile
 
 st.set_page_config(
     page_title="DarkCut AI",
@@ -8,7 +10,7 @@ st.set_page_config(
 st.title("🎬 DarkCut AI")
 st.subheader("Sua IA de vídeos e cortes")
 
-st.write("Transforme vídeos longos em Shorts, Reels e TikToks.")
+st.write("Envie um vídeo e a IA vai transcrever o conteúdo.")
 
 video = st.file_uploader(
     "📤 Envie seu vídeo",
@@ -19,7 +21,19 @@ if video:
     st.success("Vídeo recebido! 🚀")
     st.video(video)
 
-    st.info(
-        "🤖 O DarkCut AI está pronto para receber "
-        "o sistema de análise automática de cortes."
-    )
+    if st.button("🤖 Analisar vídeo"):
+        with st.spinner("🧠 Analisando o vídeo..."):
+            with tempfile.NamedTemporaryFile(
+                delete=False,
+                suffix=".mp4"
+            ) as arquivo:
+                arquivo.write(video.read())
+                caminho = arquivo.name
+
+            modelo = whisper.load_model("tiny")
+            resultado = modelo.transcribe(caminho)
+
+        st.success("✅ Análise concluída!")
+
+        st.subheader("📝 Transcrição")
+        st.write(resultado["text"])
